c0d5bd3b797ef8a1ec01f63126ea2edea9610a25
mahasachan <mahasachan.sa@ku.th>
2024-03-04 20:55:04 +0700

    remove button add boat

--- app/AdminNav.tsx
+++ app/AdminNav.tsx
@@ -4,12 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 function AdminNav() {
     return (
-        <div className="w-full flex justify-center pb-2 drop-shadow-lg">
-            <button className="btn bg-green-300 flex flex-row">
-                <FontAwesomeIcon icon={faPlus} size="xl" />
-                <p>เพิ่มเรือเข้าระบบ</p>
-            </button>
-        </div>
+        <div className="w-full bg-white h-14 flex justify-center pb-2 drop-shadow-lg"></div>
     )
 }
 
